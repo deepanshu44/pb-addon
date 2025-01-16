493743b79a3dacc7c367e9b90cf2aa1366169baf
deepanshu <62979448+deepanshu44@users.noreply.github.com>
2025-01-17 00:53:26 +0530

    installation bug fixed

--- background.js
+++ background.js
@@ -20,26 +20,32 @@ function clickListener() {
     
 }
 
-function handleUpdated(tabId, changeInfo, tabInfo) {
-    if (changeInfo.status === "complete") {
-	// NOTE: page can be refreshed multiple times by the user
-	browser.tabs
+function addonInitialization() {
+    browser.tabs
         .query({
             currentWindow: true,
             active: true,
         })
-            .then((tabs) => {
-		browser.tabs.sendMessage(tabs[0].id, {status:"DOMLoaded"}).catch(onError)
-		if (addonLoaded === false) {
-		    // when tab opened for first time
-		    browser.browserAction.onClicked.addListener(clickListener)
-		    addonLoaded = true
-		}
-	    })
+        .then((tabs) => {
+	    browser.tabs.sendMessage(tabs[0].id, {status:"DOMLoaded"}).catch(onError)
+	    if (addonLoaded === false) {
+		// when tab opened for first time
+		browser.browserAction.onClicked.addListener(clickListener)
+		addonLoaded = true
+	    }
+	})
+}
+function handleInstallation() {
+    addonInitialization()
+}
+function handleUpdated(tabId, changeInfo, tabInfo) {
+    if (changeInfo.status === "complete") {
+	// NOTE: page can be refreshed multiple times by the user
+	addonInitialization()
     }
-    
 }
 
 let addonLoaded = false;
 browser.tabs.onUpdated.addListener(handleUpdated);
+browser.runtime.onInstalled.addListener(handleInstallation)
 // todo: deal with addon activation when window out of focus
